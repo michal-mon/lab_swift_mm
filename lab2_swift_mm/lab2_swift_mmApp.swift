6683987a5a25ee68b6c8c1714bf0272adfd57d57
x <x>
2024-04-27 17:03:44 +0200

    Initial Commit

---
+++ lab2_swift_mm/lab2_swift_mmApp.swift
@@ -0,0 +1,17 @@
+//
+//  lab2_swift_mmApp.swift
+//  lab2_swift_mm
+//
+//  Created by student on 27/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct lab2_swift_mmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
